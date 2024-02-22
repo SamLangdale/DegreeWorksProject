a6ca8d580a82e1d9c0f2019eb3e4f6c8fd0a2c9d
SamLangdale <samlangdale13@gmail.com>
2024-02-22 14:01:42 -0500

    fixing JSON

---
+++ uuid.java
@@ -0,0 +1,12 @@
+import java.util.UUID;
+public class uuid {
+
+    public static void main(String[] args) {
+        
+
+    String uuid = UUID.randomUUID().toString();
+    System.out.println(uuid);
+    }
+
+    
+}

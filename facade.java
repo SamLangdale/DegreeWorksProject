efe6e25819d03e2203a4135a37910e4ef79b6286
TannerSimpson <111939959+TannerSimpson@users.noreply.github.com>
2024-02-26 11:45:54 -0500

    Create facade.java
    Skeleton for the facade class

---
+++ facade.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class facade extends uuid {
+
+    private User currentUser;
+
+    public User login(String userName, String password, UserType userType) {
+    
+    }
+
+    public void createSchedule(ArrayList<Course> takenCourses, ArrayList<Course> currentCourses,
+                               ArrayList<Course> requiredCourses, ArrayList<PreReq> prereqs,
+                               Major major, ArrayList<Grade> grades) {
+        
+    }
+
+    public void logout() {
+        
+    }
+
+    public void selectMajor() {
+        
+    }
+
+    public void viewMajorMap(Major major, ArrayList<CourseList> takenCourses,
+                             ArrayList<CurrentCourse> currentCourses,
+                             ArrayList<CourseList> requiredCourses, ArrayList<PreReq> preReqs) {
+    }
+
+    public void viewGrades() {
+        
+    }
+
+    public void selectStudent() {
+
+    }
+
+}

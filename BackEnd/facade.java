808110140e15b4f6a9d366024e1c3aeb368ae1b0
Tanner Simpson <tfs2@domain.com>
2024-02-27 14:02:45 -0500

    facade

--- BackEnd/facade.java
+++ BackEnd/facade.java
@@ -1,11 +1,17 @@
+package BackEnd;
+
 import java.util.ArrayList;
 
 public class facade extends uuid {
 
     private User currentUser;
+    private String userName;
+    private String password;
 
     public User login(String userName, String password, UserType userType) {
-    
+        this.userName = userName;
+        this.password = password;
+
     }
 
     public void createSchedule(ArrayList<Course> takenCourses, ArrayList<Course> currentCourses,
@@ -25,6 +31,9 @@ public class facade extends uuid {
     public void viewMajorMap(Major major, ArrayList<CourseList> takenCourses,
                              ArrayList<CurrentCourse> currentCourses,
                              ArrayList<CourseList> requiredCourses, ArrayList<PreReq> preReqs) {
+
+        this.major = major;
+
     }
 
     public void viewGrades() {

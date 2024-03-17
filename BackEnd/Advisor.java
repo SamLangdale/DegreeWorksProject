b6db1f97111232ca143f57e04c9b81a61da10e3f
reallyrj <112011049+reallyrj@users.noreply.github.com>
2024-03-17 15:25:07 -0400

    Update Advisor.java

--- BackEnd/Advisor.java
+++ BackEnd/Advisor.java
@@ -1,4 +1,5 @@
-package BackEnd; 
+package BackEnd;
+
 import java.util.ArrayList;
 import java.util.UUID;
 
@@ -21,32 +22,36 @@ public class Advisor extends User {
     public ArrayList<Student> getAssignedStudents() {
         return assignedStudents;
     }
+
+    // Add note to a student
+    public void addNote(Student student) {
+        //todo: Add note functionality
+    }
+
+    // Search for a student by last name, first name, and student ID
+    public boolean search(String lastName, String firstName, int studentID) {
+        //todo: Search functionality
+        return false; // Placeholder return
+    }
+
+    // Remove a note for a student
+    public void removeNote(Student student) {
+        //todo: Remove note functionality
+    }
+
+    // Edit a note for a student
+    public void editNote(Student student) {
+        //todo: Edit note functionality
+    }
+
+    // Select a student
+    public boolean selectStudent(Student student) {
+        //todo: Select student functionality
+        return false; // Placeholder return
+    }
+
+    // View warnings for a student
+    public void viewWarnings(Student student) {
+        //todo: View warnings functionality
+    }
 }
-
-    // public boolean addNotes(Student student) {
-//     //todo   
-//     return //whatever
-// }
-// public boolean Search(String LastName, 
-// String FirstName, int StudentID) {
-//     //todo
-//     return //whatever
-
-// }
-// public boolean removenote(Student Student){
-//     //todo
-//     return // whatever
-// }
-// public boolean editNote(Student Student) {
-//     //todo
-//     return //whatever
-// }
-// public boolean selectStudent(Student student) {
-//     //todo
-//     return // whatever
-// }
-// public boolean viewWarnings(Student student) {
-//     //todo
-//     return //whatever
-// } 
-
